e6936a974728b6d2d3e8a0a8089dbc452af2d66e
admin <alex.belous.1995@mail.ru>
2023-12-20 00:16:11 +0300

    Типы данных.Переменные

---
+++ lesson1_2/levelA/a1/Run.java
@@ -0,0 +1,23 @@
+package lesson1_2.levelA.a1;
+
+public class Run {
+    public static void main(String[] args) {
+        int a = 1;
+        System.out.println(a);
+        double b = 2;
+        System.out.println(b);
+        byte c = 3;
+        System.out.println(c);
+        short d = 4;
+        System.out.println(d);
+        long e = 5;
+        System.out.println(e);
+        float f = 6;
+        System.out.println(f);
+        char t = 'v';
+        System.out.println(t);
+        boolean h = true;
+        System.out.println(h);
+
+    }
+}
