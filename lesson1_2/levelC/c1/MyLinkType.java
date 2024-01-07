8d5e7e7e4e41dc712360ef64b05fac133108c09c
admin <alex.belous.1995@mail.ru>
2024-01-07 22:03:55 +0300

    Типы данных.Переменные.Неявное преобразование.Чиcло pi в java.

--- lesson1_2/levelC/c1/MyLinkType.java
+++ lesson1_2/levelC/c1/MyLinkType.java
@@ -1,7 +1,5 @@
 package lesson1_2.levelC.c1;
 
 public class MyLinkType {
-    public static void main(String[] args) {
 
-    }
 }
--- lesson1_2/levelC/c2/Run.java
+++ lesson1_2/levelC/c2/Run.java
@@ -8,42 +8,6 @@ package lesson1_2.levelC.c2;
  */
 public class Run {
     public static void main(String[] args) {
-        Enum manthValue = new Enum();
-        switch (manthValue.January) {
-            case February:
-                System.out.println("Зима");
-                break;
-            case March:
-                System.out.println("Весна");
-                break;
-            case April:
-                System.out.println("Весна");
-                break;
-            case May:
-                System.out.println("Весна");
-                break;
-            case July:
-                System.out.println("Лето");
-                break;
-            case June:
-                System.out.println("Лето");
-                break;
-            case August:
-                System.out.println("Лето");
-                break;
-            case September:
-                System.out.println("Осень");
-                break;
-            case October:
-                System.out.println("Осень");
-                break;
-            case November:
-                System.out.println("Осень");
-            case December:
-                System.out.println("Зима");
-                break;
-            /*default:
-                System.out.println("Нет такого месяца");*/
-        }
+
     }
 }
